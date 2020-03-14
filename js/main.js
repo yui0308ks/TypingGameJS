289615a3438a3f32f99fb99a86961e0c7d78f3b6
yui0308ks <kazuyu03081@gmail.com>
2020-03-14 23:22:23 +0900

    誤作動防止

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
   let miss = 0;
   const timeLimit = 3 * 1000;
   let startTime;
+  let isPlaying = false;
 
   const target = document.getElementById('target');
   const scoreLabel = document.getElementById('score');
@@ -38,6 +39,8 @@
     }, 10);
 
     if (timeLeft < 0){
+      isPlaying = false;
+
       clearTimeout(timeoutId);
       timerLabel.textContent = '0.00';
       setTimeout(() => {
@@ -48,13 +51,19 @@
 
 
   window.addEventListener('click', () => {
+    if (isPlaying === true) {
+      return;
+    }
+    isPlaying = true;
     target.textContent = word;
     startTime = Date.now();
     updateTimer();
   });
 
   window.addEventListener('keydown', e => {
-    console.log(e.key)
+    if (isPlaying !== true) {
+      return;
+    }
     if (e.key === word[loc]) {
       console.log('score');
       loc++;
